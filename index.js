7a632a2a053e1f793ae4a56c1a22fdae822642b0
Philipp Alferov <philipp.alferov@gmail.com>
2015-10-12 19:21:57 -0400

    Implement ability to generate https URLs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 var isString = require('is-string');
-
+var ghParser = require('parse-github-url');
 /**
  * getGithubUrl
  *
@@ -11,10 +11,13 @@ var isString = require('is-string');
 
 module.exports = function getGithubUrl(options) {
   var url = options.url;
-  var protocol = options.protocol;
+  var startLine = options.protocol === 'ssh' ? 'git@github.com' : 'https://github.com/';
 
   if (!isString(url)) {
     throw TypeError('URL must be a string');
   }
-  
+
+  var parsed = ghParser(url);
+
+  return startLine + parsed.repopath;
 };
--- test/test.js
+++ test/test.js
@@ -2,11 +2,20 @@
 var chai = require('chai');
 var expect = chai.expect;
 var getGithubUrl = require('../index.js');
-var current, expected;
+var expected = 'https://github.com/facebook/react';
 
 describe('get-github-url', function() {
   it('should throw an error if url is not a string', function() {
     expect(getGithubUrl.bind(null, { url: {} }))
       .to.throw(/must be a string/);
+  });
+
+  it('should get valid https url', function() {
+    expect(getGithubUrl({ url: 'facebook/react' }))
+      .to.be.equal(expected);
+
+    expect(getGithubUrl({ url: 'git@github.com:facebook/react.git' }))
+      .to.be.equal(expected);
+
   });
 });
